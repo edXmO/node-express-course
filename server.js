c3e33af254f392b6f5f5f682c462ce7e5d5627e7
Eduardo <ex.romeroma@gmail.com>
2020-11-26 11:41:15 +0100

    add dynamic GET route

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const port = 8000;
 // First line gives access to express library
 // Next creates an instance of the express constructor, bamed app
 
@@ -17,6 +17,19 @@ app.get('/users', (req, res) => {
     })
 })
 
-app.listen(8000, () => {
-    console.log("server is running");
+//In Express, words with a colon in front of them in 
+// the url are treated as variables. 
+// You can access the value of each variable through req.params
+
+app.get('/users/:id', (req, res) => {
+    console.log(req.params.id)
+    res.json({
+        success: true,
+        message: "got one user",
+        user: req.params.id
+    })
+})
+
+app.listen(port, () => {
+    console.log(`server is running on http://localhost:${port}`);
 })
